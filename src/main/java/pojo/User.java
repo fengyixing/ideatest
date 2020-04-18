b0e7a2d4b1edb2f28d6b98e7f2e3604a3616180c
admin <1286374206@qq.com>
2020-04-18 16:12:08 +0800

    添加工程

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,13 @@
+/**
+ * @FileName: User
+ * @Author: ASUS
+ * @Date: 2020/4/18 16:06
+ * @Description:
+ */
+package pojo;
+
+public class User {
+    private String name;
+    private String email;
+
+}
